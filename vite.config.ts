a31754f1e6af53096a76cf76944139f98e9d38ae
Carter Smithhart <carter.smithhart@gmail.com>
2023-10-29 12:14:59 -0700

    Testing workaround using strictRequires
    https://github.com/vitejs/vite/issues/13672#issuecomment-1784110536
    
    Still fails producing a deterministic build:
    npm run build
    mv build build1
    npm run build
    diff -qr build build1

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,24 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	build: {
-		rollupOptions: {
-			// Trying potential workaround for
-			// https://github.com/vitejs/vite/issues/10506
-			//
-			// Still fails with:
-			// npm run build
-			// mv build build1
-			// npm run build
-			// diff -qr build build1
-			maxParallelFileOps: 1
+		//rollupOptions: {
+		// Trying potential workaround for
+		// https://github.com/vitejs/vite/issues/10506
+		//
+		// Still fails with:
+		// npm run build
+		// mv build build1
+		// npm run build
+		// diff -qr build build1
+		//
+		//maxParallelFileOps: 1
+		//}
+		// Trying workaround from
+		// https://github.com/vitejs/vite/issues/13672#issuecomment-1784110536
+		//
+		// Still fails using test
+		commonjsOptions: {
+			strictRequires: true
 		}
 	}
 });
